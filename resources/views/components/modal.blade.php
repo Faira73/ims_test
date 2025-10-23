b7d3e0ccc31b3dabbb606dcce1f4813c43106796
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-10-23 10:34:56 +0300

    Create new component for modals

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,11 @@
+<div id="{{$id}}" class="modal" style="display: none;">
+    <div class="modal-content">
+        <h2>{{$title}}</h2>
+        <form action="{{$action}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            {{$slot}}
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+</div>
