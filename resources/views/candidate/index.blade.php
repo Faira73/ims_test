0226aa9bfa47981744945f4d874a51daee65b281
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-10-23 12:21:00 +0300

    Move javascrip modal  code to js/components/modal.js

--- resources/views/candidate/index.blade.php
+++ resources/views/candidate/index.blade.php
@@ -106,28 +106,4 @@
             </form>
         </div>
     </div>
-
-    <script>
-        // Get elements
-        const modal = document.getElementById('popupModal');
-        const btn = document.getElementById('openPopupBtn');
-        const closeBtn = document.querySelector('.close');
-
-        // Open modal
-        btn.onclick = function() {
-            modal.style.display = 'block';
-        }
-
-        // Close modal when X is clicked
-        closeBtn.onclick = function() {
-            modal.style.display = 'none';
-        }
-
-        // Close modal when clicking outside
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = 'none';
-            }
-        }
-    </script>
 </x-layout>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,4 +3,34 @@
         <x-navigation-sidebar/>
     </x-slot>
     <h1>Questions</h1>
+    <div class="top-navigation">
+        <button onclick="openModal('addQuestion')">Add Question</button>
+        <button onclick="openModal('addCategory')">Add Category</button>
+    </div>
+    <div style="background-color: #F6F7F9">
+        
+    </div>
+
+
+
+
+
+
+
+
+
+    {{-- button modals --}}
+    <x-modal id="addQuestion" title="Add New Question" action="{{ route('questions.store') }}">
+        <label>Question Text</label>
+        <input type="text" name="question_text" required>
+
+        <label>Category</label>
+        <select name="category_id">
+            @foreach ($categories as $cat)
+                <option value="{{ $cat->id }}">{{ $cat->name }}</option>
+            @endforeach
+        </select>
+    </x-modal>
+
+
 </x-layout>
