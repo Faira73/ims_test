97146a6f0d27df9507e29a8603477a7c52b255b3
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-11-02 13:16:49 +0300

    Add mass assignment

--- app/Models/EvaluationScore.php
+++ app/Models/EvaluationScore.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class EvaluationScore extends Model
-{
+{   
+    protected $fillable = ['evaluation_id', 'criterion_id', 'score', 'comment'];
     public function evaluation() 
     {
         return $this->belongsTo(Evaluation::class);     
