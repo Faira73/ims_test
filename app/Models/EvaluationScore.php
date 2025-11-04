db8f85f5cfa119bd95cedb8b30781c1c52ba1616
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-11-04 13:10:34 +0300

    Fix typo

--- app/Models/EvaluationScore.php
+++ app/Models/EvaluationScore.php
@@ -9,11 +9,13 @@ class EvaluationScore extends Model
     protected $fillable = ['evaluation_id', 'criterion_id', 'score', 'comment'];
     public function evaluation() 
     {
-        return $this->belongsTo(Evaluation::class);     
+        return $this->belongsTo(Evaluation::class, 'evaluation_id');     
     }
 
     public function criteria()
     {
         return $this->belongsTo(EvaluationCriteria::class, 'criterion_id');
     }
+
+    
 }
