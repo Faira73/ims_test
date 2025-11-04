0f41541c4f4f1b45637ba64b023ebec27b6e01f0
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-11-04 10:50:45 +0300

    Refactor store method

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -26,33 +26,44 @@ class EvaluationController extends Controller
 
         public function store(Request $request, Interview $interview)
     {
-        $validated = $request->validate([
-            'overall_note' => 'nullable|string',
-            'overall_score' => 'nullable|numeric|min:0|max:5',
-            'scores' => 'required|array',
-            'scores.*.criterion_id' => 'required|exists:evaluation_criteria,id',
-            'scores.*.score' => 'required|integer|min:1|max:5',
-            'scores.*.comment' => 'nullable|string',
+        $user = Auth::id();
+
+        if (!$interview ->interviewers->contains($user)){
+            abort(403, 'Unauthorized');
+        }
+
+        $validate = $request->validate([
+            'ratings' => 'required | array', 
+            'ratings.*' => 'required | integer | min:1 | max:5',
+            'comments' => 'nullable | array', 
+            'comment.*' => 'lullable | string | max: 255'
         ]);
 
-        $evaluation = Evaluation::create([
+        $existingEvaluation = Evaluation::where('interview_id', $interview->id)
+            ->where('evaluator_id', $user)
+            ->first();
+        
+        if($existingEvaluation){
+            return redirect()->back()->with('error', 'You have already submitted your evaluation');
+        }
+
+         $evaluation = Evaluation::create([
             'interview_id' => $interview->id,
-            'evaluator_id' => Auth::id(),
-            'overall_note' => $validated['overall_note'] ?? null,
-            'overall_score' => $validated['overall_score'] ?? null,
+            'evaluator_id' => $user,
+            'overall_score' => null, // you can calculate later if needed
+            'overall_note' => null,
         ]);
-
-        foreach ($validated['scores'] as $scoreData) {
-            EvaluationScore::create([
-                'evaluation_id' => $evaluation->id,
-                'criterion_id'  => $scoreData['criterion_id'],
-                'score'         => $scoreData['score'],
-                'comment'       => $scoreData['comment'] ?? null,
+        
+        foreach ($validate['ratings'] as $criterionId => $score) {
+        EvaluationScore::create([
+            'evaluation_id' => $evaluation->id,
+            'criterion_id' => $criterionId,
+            'score' => $score,
+            'comment' => $validate['comments'][$criterionId] ?? null,
             ]);
         }
 
-        return redirect()->route('interview.show', $interview->id)
-            ->with('success', 'Evaluation submitted successfully.');
+        return redirect()->back()->with('success', 'Evaluation submitted successfully.');
     }
      public function edit(Evaluation $evaluation)
     {
