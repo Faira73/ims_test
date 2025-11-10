56f3fe6529ff68b6b3048130d7141278bdcc0b69
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-11-10 12:58:50 +0300

    Hide employee tab from users who are not admin

--- resources/views/components/navigation-sidebar.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/candidates/">Candidates</a></li>
-    <li><a href="/employee/status/approved">Employees</a></li>
-    <li><a href="/employee/status/pending">Pending Employees</a></li>
-    <li><a href="/interviews">Interviews</a></li>
-    <li><a href="/questions">Questions Bank</a></li>
-</ul>
